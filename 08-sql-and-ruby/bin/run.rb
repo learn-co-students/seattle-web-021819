1f58473dbf7432e56f73a6176c7af473a620c75f
github username <github email address>
2019-02-27 15:49:13 -0800

    course student enrollment

--- 08-sql-and-ruby/bin/run.rb
+++ 08-sql-and-ruby/bin/run.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/env ruby
 require_relative '../config/environment'
 
-TweetsApp.new.call
+sql = <<-SQL
+SELECT name, title, grade_percent
+FROM enrollments
+JOIN students
+ON enrollments.student_id = students.id
+JOIN courses
+ON enrollments.course_id = courses.id
+SQL
+
+students = Student.all
+courses = Course.all
+enrollments = Enrollment.all
+
+Student.find_by_id 1
+
+binding.pry
--- 08-sql-and-ruby/config/environment.rb
+++ 08-sql-and-ruby/config/environment.rb
@@ -1,11 +1,12 @@
-require 'bundler'
-Bundler.require
+require 'pry'
+require 'sqlite3'
 
 DB = {
-  conn: SQLite3::Database.new('db/twitter.db')
+  conn: SQLite3::Database.new('db/simpsons.db')
 }
 
 DB[:conn].results_as_hash = true
 
-require_relative '../lib/tweet.rb'
-require_relative '../lib/tweets_app.rb'
+require_relative '../lib/course'
+require_relative '../lib/student'
+require_relative '../lib/enrollment'
