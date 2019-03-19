56ff4201857ce997048e0321a5214b0944b11fdc
Niky Morgan <niky@flatironschool.com>
2019-03-19 12:24:17 -0700

    assocations

--- 20-rails-form-associations/config/routes.rb
+++ 20-rails-form-associations/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  # resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :burgers
-  # get '/burger/:id', to: 'burgers#show'
+  # get '/burgers/:id', to: 'burgers#show'
+  # delete '/burgers/:id', to: 'burgers#destroy'
 end
