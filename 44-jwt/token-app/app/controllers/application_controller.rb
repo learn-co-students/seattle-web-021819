2f0bc5452b6badb2120c530842e28764f7c711d0
Steve Geluso <github email address>
2019-05-02 14:42:54 -0700

    jwt

---
+++ 44-jwt/token-app/app/controllers/application_controller.rb
@@ -0,0 +1,67 @@
+class ApplicationController < ActionController::API
+  before_action :authorized
+  skip_before_action :authorized, only: [:create]
+
+  def create
+    @user = User.find_by(username: user_login_params[:username])
+    #User#authenticate comes from BCrypt
+    if @user && @user.authenticate(user_login_params[:password])
+      # encode token comes from ApplicationController
+      token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  def user_login_params
+    # params { user: {username: 'Chandler Bing', password: 'hi' } }
+    params.require(:user).permit(:username, :password)
+  end
+
+  def encode_token(payload)
+    # payload => { beef: 'steak' }
+    JWT.encode(payload, 'my_s3cr3t')
+    # jwt string: "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+  end
+
+  def decoded_token(token)
+    # token => "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+
+    JWT.decode(token, 'my_s3cr3t')[0]
+    # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+    # [0] gives us the payload { "beef"=>"steak" }
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+end
